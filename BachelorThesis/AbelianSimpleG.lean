9a018dea2de40313447e175b3a32fc5655deda00
Lingyin Luo <Luo.Lingyin@campus.lmu.de>
2025-11-07 14:02:28 +0100

    style: change the code style of AbelianSimpleG close to mathlib

--- BachelorThesis/AbelianSimpleG.lean
+++ BachelorThesis/AbelianSimpleG.lean
@@ -11,20 +11,21 @@ variable {G : Type*} [CommGroup G] [IsSimpleGroup G]
 -- **this small lemma might be a PR to mathlib**
 omit [IsSimpleGroup G] in
 @[simp]
-lemma finiteCyclicIffFiniteCarrier (g : G) :
+lemma finiteCyclic_iff_finiteCarrier (g : G) :
     Finite (Subgroup.zpowers g) = ((Subgroup.zpowers g) : Set G).Finite := by
   rfl
 
 /-! Using g to build a finite cyclic group-/
 omit [IsSimpleGroup G] in
-lemma finiteOfFinOrderGenerator (g : G) (hp : ¬orderOf g = 0) :
+lemma finite_finOrder_zpow (g : G) (hp : ¬orderOf g = 0) :
     Finite (Subgroup.zpowers g) := by
   simp_all
 
 #check isOfFinOrder_iff_pow_eq_one
+#check isOfFinOrder_iff_zpow_eq_one
 
 /-! Finitness of an abelian simple group -/
-lemma FiniteOfSimpleCyclic : (Finite G) := by
+lemma finite_abelianSimple : (Finite G) := by
   obtain ⟨g, hg⟩ := -- G is cyclic
     (isCyclic_iff_exists_zpowers_eq_top : IsCyclic G ↔ _).mp (by infer_instance)
   by_contra hnf
@@ -32,7 +33,7 @@ lemma FiniteOfSimpleCyclic : (Finite G) := by
   -- using zpowers g and Infinite G to get that orderOg g is 0
   have horder : orderOf g = 0 := by
     by_contra h0
-    have hfin : Finite (Subgroup.zpowers g) := by exact finiteOfFinOrderGenerator g h0
+    have hfin : Finite (Subgroup.zpowers g) := by exact finite_finOrder_zpow g h0
     haveI : Finite ((⊤ : Subgroup G)) := by simpa [hg] using hfin
     -- transfer the Finiteness through surjection into G，to get contradiction
     have hfinG : Finite G :=
@@ -56,13 +57,11 @@ lemma FiniteOfSimpleCyclic : (Finite G) := by
       have hne0 : (2 * z - 3 : ℤ) ≠ 0 := by omega
       by_cases hg1 : g = 1
       · simp_all only [Subgroup.zpowers_one_eq_bot, bot_ne_top]
-      · have eqpow : g ^ (2 * z - 3 : ℤ) = g ^ (0 : ℤ) := by
-          simpa [zpow_zero] using h1
-        have : ¬ IsOfFinOrder g := by apply orderOf_eq_zero_iff.mp horder
-        have tors : IsOfFinOrder g := by
+      · have : ¬ IsOfFinOrder g := by apply orderOf_eq_zero_iff.mp horder
+        have finOrder_g : IsOfFinOrder g := by
           refine (isOfFinOrder_iff_zpow_eq_one).mpr ?_
           exact ⟨2 * z - 3, hne0, h1⟩
-        exact this tors
+        exact this finOrder_g
     simp_all only [not_finite_iff_infinite, orderOf_eq_zero_iff, Subgroup.mem_top,
       not_true_eq_false]
   have hneq_bot : H ≠ ⊥ := by
@@ -80,21 +79,22 @@ lemma FiniteOfSimpleCyclic : (Finite G) := by
           exact Nat.zero_ne_one this
       have h02 : (0 : ℕ) = 2 := by rw [← horder]; exact hh
       exact (by decide : (0 : ℕ) ≠ 2) h02
-    have hmemZ : g ^ 2 ∈ H := by
+    have hmemg2 : g ^ 2 ∈ H := by
       refine (Subgroup.mem_zpowers_iff).mpr ?_
       exact ⟨1 , by simp⟩
-    rw [hbot] at hmemZ
-    exact ne hmemZ
+    rw [hbot] at hmemg2
+    exact ne hmemg2
   have := IsSimpleGroup.eq_bot_or_eq_top_of_normal (H := H) Hnorm
   rcases this <;> contradiction
 
 /-！Main theorem : if G is an abelian simple group then G ≃* Zₚ for some prime p. -/
-theorem AbelianSimpleGroupIsoOfZp : ∃ p : ℕ , (Nat.Prime p) ∧ Nonempty (Additive G ≃+ ZMod p) := by
+theorem abelianSimpleG_isoOfZMod_prime :
+    ∃ p : ℕ , (Nat.Prime p) ∧ Nonempty (Additive G ≃+ ZMod p) := by
   have hcyc : IsCyclic G := by infer_instance -- a simple abelian group is cyclic
   rcases (isCyclic_iff_exists_zpowers_eq_top.mp hcyc) with ⟨g, hg⟩ -- get the generator
   let p : ℕ := orderOf g -- get the order of g, not assuming g ≠ 0 here
   have orderG : Nat.card G = p := by exact Eq.symm (orderOf_eq_card_of_zpowers_eq_top hg)
-  have FinG : Finite G := by exact FiniteOfSimpleCyclic
+  have FinG : Finite G := by exact finite_abelianSimple
   -- using the mathlib theorem about prime_card
   have hp : Nat.Prime p := by rw [← orderG]; apply IsSimpleGroup.prime_card
   use p
